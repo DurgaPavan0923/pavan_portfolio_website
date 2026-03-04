9880d5372e832107cab951678f2aca61115f728d
Pavan Kumar <rdpkumar2308@gmail.com>
2026-03-04 05:56:39 +0530

    Update seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,1 +1,13 @@
-
+export const structuredData = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  name: "Rajana Durga Pavan Kumar",
+  url: "https://yourdomain.vercel.app",
+  sameAs: [
+    "https://github.com/DurgaPavan0923",
+    "https://www.linkedin.com/in/rajana-durga-pavan-kumar-432248298",
+    "https://x.com/PavanKu74939078"
+  ],
+  jobTitle: "AI/ML Engineer",
+  email: "rdpkumar2308@gmail.com"
+};
