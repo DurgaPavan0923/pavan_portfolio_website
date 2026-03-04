4ae4ce943eb1c2ce0b974def22b533f41d805dfd
Pavan Kumar <rdpkumar2308@gmail.com>
2026-03-04 06:01:16 +0530

    Add sitemap function to generate URLs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,7 @@
-
+export default function sitemap() {
+  return [
+    { url: "https://yourdomain.vercel.app" },
+    { url: "https://yourdomain.vercel.app/blog" },
+    { url: "https://yourdomain.vercel.app/resume" }
+  ];
+}
