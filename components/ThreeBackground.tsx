6bef3613a21ac91f24de4c2424467949a1d793c6
Pavan Kumar <rdpkumar2308@gmail.com>
2026-03-04 05:57:27 +0530

    Update ThreeBackground.tsx

--- components/ThreeBackground.tsx
+++ components/ThreeBackground.tsx
@@ -1,1 +1,13 @@
+"use client";
+import { Canvas } from "@react-three/fiber";
+import { Stars } from "@react-three/drei";
 
+export default function ThreeBackground() {
+  return (
+    <div className="fixed inset-0 -z-10">
+      <Canvas>
+        <Stars radius={100} depth={50} count={4000} factor={4} fade />
+      </Canvas>
+    </div>
+  );
+}
