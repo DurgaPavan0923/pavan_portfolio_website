72101297f87aa8752e1d2bdd1aec2fa257bf3ef0
Pavan Kumar <rdpkumar2308@gmail.com>
2026-03-04 06:00:53 +0530

    Update page.tsx

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,1 +1,10 @@
-
+export default function ResumePage() {
+  return (
+    <div className="h-screen">
+      <iframe
+        src="/resume.pdf"
+        className="w-full h-full"
+      />
+    </div>
+  );
+}
