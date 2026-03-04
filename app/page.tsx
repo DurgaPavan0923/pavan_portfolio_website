b08457d66d5d228bcb57fdf1d6662a36e8c99f5f
Pavan Kumar <rdpkumar2308@gmail.com>
2026-03-04 06:32:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,27 @@
 "use client";
 
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Experience from "@/components/Experience";
-import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
-import BlogPreview from "@/components/BlogPreview";
-import Footer from "@/components/Footer";
-import Chatbot from "@/components/Chatbot";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Experience from "../components/Experience";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import BlogPreview from "../components/BlogPreview";
+import Footer from "../components/Footer";
+import Chatbot from "../components/Chatbot";
 
 export default function Home() {
   return (
     <main className="relative">
-      
-      {/* Navigation */}
       <Navbar />
-
-      {/* Hero Section */}
-      <section id="home">
-        <Hero />
-      </section>
-
-      {/* About */}
-      <section id="about" className="py-32 px-6 max-w-5xl mx-auto">
-        <About />
-      </section>
-
-      {/* Experience */}
-      <section id="experience" className="py-32 bg-black/40 px-6">
-        <Experience />
-      </section>
-
-      {/* Projects */}
-      <section id="projects" className="py-32 px-6 max-w-6xl mx-auto">
-        <Projects />
-      </section>
-
-      {/* Skills */}
-      <section id="skills" className="py-32 bg-black/40 px-6">
-        <Skills />
-      </section>
-
-      {/* Blog Preview */}
-      <section id="blog" className="py-32 px-6 max-w-5xl mx-auto">
-        <BlogPreview />
-      </section>
-
-      {/* Footer */}
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Skills />
+      <BlogPreview />
       <Footer />
-
-      {/* AI Chatbot Floating */}
       <Chatbot />
-
     </main>
   );
 }
