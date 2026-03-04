b93ca6473a8ce2887a71378a0a945d5f96724283
Pavan Kumar <rdpkumar2308@gmail.com>
2026-03-04 05:57:55 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,18 @@
+"use client";
+import Link from "next/link";
 
+export default function Navbar() {
+  return (
+    <nav className="fixed w-full backdrop-blur-md bg-black/40 p-5 z-50">
+      <div className="max-w-6xl mx-auto flex justify-between">
+        <h1 className="font-bold text-xl">Pavan</h1>
+        <div className="space-x-6 hidden md:block">
+          <Link href="#about">About</Link>
+          <Link href="#projects">Projects</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/resume">Resume</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
